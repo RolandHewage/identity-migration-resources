1818e49057f27b768930e0a679b490a4beafaddc
sandushi <sandushidileka2@gmail.com>
2022-08-03 16:01:52 +0530

    Format the code

--- components/org.wso2.is.migration/migration-resources/6.0.0/dbscripts/step5/um/postgresql.sql
+++ components/org.wso2.is.migration/migration-resources/6.0.0/dbscripts/step5/um/postgresql.sql
@@ -7,18 +7,18 @@ BEGIN
     JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
     JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
     WHERE constraint_type ='UNIQUE' AND tc.table_name = 'um_user' AND kcu.column_name = 'um_user_id' GROUP BY tc.constraint_name ;
-
+    
     IF con_name IS NOT NULL THEN
-    	EXECUTE 'ALTER TABLE um_user DROP CONSTRAINT IF EXISTS '|| con_name || ';';
+        EXECUTE 'ALTER TABLE um_user DROP CONSTRAINT IF EXISTS '|| con_name || ';';
     END IF;
-
+    
     ALTER TABLE um_user ADD CONSTRAINT um_user_um_user_id_key UNIQUE(um_user_id);
-
+    
     IF NOT EXISTS (
         SELECT * FROM information_schema.table_constraints AS tc
-		JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
-		JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
-		WHERE constraint_type = 'UNIQUE' AND tc.table_name = 'um_user' AND kcu.column_name = 'um_user_name') THEN
+        JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name
+        JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name
+        WHERE constraint_type = 'UNIQUE' AND tc.table_name = 'um_user' AND kcu.column_name = 'um_user_name') THEN
             ALTER TABLE um_user ADD CONSTRAINT um_user_um_user_name_um_tenant_id_key UNIQUE(um_user_name,um_tenant_id);
     END IF;
 END $$;
